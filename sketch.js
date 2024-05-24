367a8b102d96342646e8930fc1277594c6efb91a
HerArm <hermannwr80@gmail.com>
2024-05-24 09:16:14 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+  background("black");
+}
+
+function draw() {
+  fill("white");
+  stroke("yellow");
+  if(mouseIsPressed){rect(mouseX, mouseY, 25, 35)};
+
+}
